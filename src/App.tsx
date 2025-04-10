d83ce8a51d073507a8bc5365640f0e3b036bea31
shun okuda <okuda@ibsnet.co.jp>
2025-04-10 15:37:29 +0900

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Project from './pages/Project';
+import Contact from './pages/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/pages/Project.tsx
@@ -0,0 +1,10 @@
+const Project = () => {
+  return (
+    <div>
+      <h1>Project</h1>
+      <p>This is the Project page.</p>
+    </div>
+  )
+}
+
+export default Project;
